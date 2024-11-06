41ce92b2a8ac52b9a85dccc69aba755f466f50f6
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:36:07 -0600

    Add files via upload

---
+++ EVA2_11_NUMERO_MAYOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_11_numero_mayor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_11_NUMERO_MAYOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num,mayor;
+         Scanner captu = new Scanner(System.in);
+         do{
+              System.out.println("Escribir un numero que no sea negativo");
+             num=captu.nextInt();
+         if (menor >=1)
+         if(menor<mayor)    
+             
+             
+  
+             }while(menor > 1 && menor <=mayor)
+         }
+}
+        
+           
+         
+
